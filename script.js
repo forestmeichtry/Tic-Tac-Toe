7169c46e68efafbad277f1c48025b65af924a47e
forestmeichtry <fmeichtry@gmail.com>
2023-03-01 18:58:22 -0800

    -Add options button

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-import { animateBackground, initializeGame, toggleLightMode, playAgain, returnToStart } from "./displayController.js";
+import { animateBackground, initializeGame, toggleLightMode, playAgain, returnToStart, toggleOptions } from "./displayController.js";
 
 const startButton = document.querySelector('#startButton');
 startButton.addEventListener('click', initializeGame);
-
-const lightModeButton = document.querySelector('#lightMode');
-lightModeButton.addEventListener('click', toggleLightMode);
 
 const playAgainButton = document.querySelector('#playAgain');
 playAgainButton.addEventListener('click', playAgain);
 
 const mainMenuButton = document.querySelector('#mainMenu');
 mainMenuButton.addEventListener('click', returnToStart);
+
+const optionsButton = document.querySelector('#optionsButton');
+optionsButton.addEventListener('click', toggleOptions);
 
 animateBackground();
 
